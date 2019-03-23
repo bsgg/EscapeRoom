048944553f4b58228828f0d1d782d002d3777c11
bsgg <beatriz.s.g86@gmail.com>
2019-03-23 15:52:06 +0000

    Added window interactive to leave object between rooms

---
+++ EscapeRoom/Source/EscapeRoom/Game/GameLogic/WindowInteractive.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Game/GameLogic/Interactive.h"
+#include "WindowInteractive.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPEROOM_API AWindowInteractive : public AInteractive
+{
+	GENERATED_BODY()
+	
+};
