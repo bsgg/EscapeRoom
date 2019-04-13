6e54e6bc6459c29befd1588edb56a1b513481824
bsgg <beatriz.s.g86@gmail.com>
2019-04-13 14:10:17 +0100

    Included Combination UI: Buttons and text generic widget

---
+++ EscapeRoom/Source/EscapeRoom/Game/GameLogic/Interactives/UI/CombinationUI.cpp
@@ -0,0 +1,65 @@
+
+#include "CombinationUI.h"
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
+
+
+bool UCombinationUI::Initialize()
+{
+	bool Success = Super::Initialize();
+
+	ABCArray = { "0", "1", "2", "3","4","5","6","7","8","9", "A",  "B",  "C",  "D",  "E",  "G",  "H", "I", "J", "K", "L", "M", "O", "P", "Q", "R", "S", "T", "U", "V", "W",  "X",  "Y", "Z" };
+
+	if (UpButton == nullptr) return false;
+	UpButton->OnClicked.AddDynamic(this, &UCombinationUI::OnUpButtonPressed);
+
+	if (DownButton == nullptr) return false;
+	DownButton->OnClicked.AddDynamic(this, &UCombinationUI::OnDownButtonPressed);
+
+	ResetCombination();	
+
+	CharacterText->SetText(FText::FromString(ABCArray[ChainIndex]));
+
+	return true;
+}
+
+void UCombinationUI::OnUpButtonPressed()
+{
+	ChainIndex += 1;
+
+	if (ChainIndex >= ABCArray.Num())
+	{
+		ChainIndex = 0;
+	}
+
+	if (CharacterText == nullptr) return;
+	CharacterText->SetText(FText::FromString(ABCArray[ChainIndex]));
+}
+
+void UCombinationUI::OnDownButtonPressed()
+{
+	ChainIndex -= 1;
+
+	if (ChainIndex < 0)
+	{
+		ChainIndex = ABCArray.Num() -1;
+	}
+
+	if (CharacterText == nullptr) return;
+	CharacterText->SetText(FText::FromString(ABCArray[ChainIndex]));
+}
+
+
+void UCombinationUI::ResetCombination()
+{
+	if (CharacterText == nullptr) return;
+	CharacterText->SetText(FText::FromString(""));
+	ChainIndex = 0;
+}
+
+FString UCombinationUI::GetCharacter() const
+{
+	if (CharacterText == nullptr) return "";
+
+	return CharacterText->Text.ToString();
+}
---
+++ EscapeRoom/Source/EscapeRoom/Game/GameLogic/Interactives/UI/CombinationUI.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CombinationUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPEROOM_API UCombinationUI : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	
+	UPROPERTY(meta = (BindWidget))
+	class UButton* UpButton;
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* DownButton;
+
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* CharacterText;
+
+	TArray<FString> ABCArray;
+
+	int32 ChainIndex = 0;
+
+protected:
+	virtual bool Initialize() override;
+
+	void OnUpButtonPressed();
+
+	void OnDownButtonPressed();
+
+public:
+
+	void ResetCombination();
+
+	FString GetCharacter() const;	
+	
+};
